e308adf94047c333999e8c42ccf8baa096a10e7a
Edsel Solon <Edsel.Solon@bsegroup.com>
2025-07-13 22:49:40 +0800

    Updated batch file to match new script location

---
+++ update_html.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\Users\esolon\OneDrive - Boston Semi Equipment LLC\Github Projects\Inventory"
+"C:\Users\esolon\AppData\Local\Programs\Python\Python313\python.exe" update_html.py
+pause
